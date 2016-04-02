5e62e6e5dea463af708aa96db9fa20bd468509ee
chickitosik <chickitosik@gmail.com>
2016-04-02 22:59:13 +0300

    marked CategoryMapperTest

--- src/test/java/com/salesa/dao/mapper/CategoryMapperTest.java
+++ src/test/java/com/salesa/dao/mapper/CategoryMapperTest.java
@@ -12,12 +12,15 @@ public class CategoryMapperTest {
 
     @Test
     public void testMappingCategory() throws SQLException{
+        //prepare
         CategoryMapper categoryMapper = new CategoryMapper();
         ResultSet resultSet = Mockito.mock(ResultSet.class);
         when(resultSet.getInt("id")).thenReturn(1);
         when(resultSet.getString("name")).thenReturn("room");
         when(resultSet.getInt("parentId")).thenReturn(0);
+        //when
         Category category = categoryMapper.mapRow(resultSet, 1);
+        //then
         assertEquals(1, category.getId());
         assertEquals("room", category.getName());
         assertEquals((Integer) 0, category.getParentId());
