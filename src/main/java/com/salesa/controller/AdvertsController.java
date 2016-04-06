c3d05a85bd183f7bdc3aca810249ae2690914057
Kate Guz <katherina.guz@gmail.com>
2016-04-06 03:06:35 +0300

    add pagination, test

--- src/main/java/com/salesa/controller/AdvertsController.java
+++ src/main/java/com/salesa/controller/AdvertsController.java
@@ -1,7 +1,9 @@
 package com.salesa.controller;
 
+import com.salesa.filter.AdvertFilter;
 import com.salesa.service.AdvertService;
 import com.salesa.service.CategoryService;
+import com.salesa.util.AdvertPageData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdvertsController {
@@ -21,15 +24,22 @@ public class AdvertsController {
     private AdvertService advertService;
 
     @RequestMapping("/")
-    public String home(Model model) {
+    public String home(@RequestParam(name = "page", defaultValue = "1") int page, Model model) {
         model.addAttribute("categories", categoryService.getAll());
-        model.addAttribute("adverts", advertService.getAll());
+        AdvertFilter advertFilter = new AdvertFilter();
+        advertFilter.setPage(page);
+        AdvertPageData advertPageData = advertService.get(advertFilter);
+        model.addAttribute("adverts", advertPageData.getAdverts());
         return "home";
     }
 
     @RequestMapping("/category/{categoryId}")
-    public String category(@PathVariable("categoryId") int categoryId, Model model) {
+    public String category(@PathVariable("categoryId") int categoryId, @RequestParam(name = "page", defaultValue = "1") int page, Model model) {
         log.info("Query adverts information for category with id: " + categoryId);
+        AdvertFilter advertFilter = new AdvertFilter();
+        advertFilter.setPage(page);
+        AdvertPageData advertPageData = advertService.get(advertFilter);
+        model.addAttribute("adverts", advertPageData.getAdverts());
         return "home";
     }
 }
