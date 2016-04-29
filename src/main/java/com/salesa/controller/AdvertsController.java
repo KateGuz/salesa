968974650d8312e69b5318f7fbfad8b4a3094860
Kate Guz <katherina.guz@gmail.com>
2016-04-29 22:37:26 +0300

    connecion to nbu

--- src/main/java/com/salesa/controller/AdvertsController.java
+++ src/main/java/com/salesa/controller/AdvertsController.java
@@ -1,6 +1,8 @@
 package com.salesa.controller;
 
 
+import com.salesa.CurrencyConverter;
+import com.salesa.entity.Advert;
 import com.salesa.entity.User;
 import com.salesa.filter.AdvertFilter;
 import com.salesa.security.UserSecurity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 public class AdvertsController {
@@ -30,11 +33,22 @@ public class AdvertsController {
 
 
     @RequestMapping("/")
-    public String home(@RequestParam(name = "page", defaultValue = "1") int page, Model model, HttpSession session) {
+    public String home(@RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "currency", defaultValue = "UAH") String currency, Model model, HttpSession session) throws IOException {
         model.addAttribute("categories", categoryService.getAll());
         AdvertFilter advertFilter = new AdvertFilter();
         advertFilter.setPage(page);
         AdvertPageData advertPageData = advertService.get(advertFilter);
+
+        if(currency.equals("UAH")){
+            for (Advert advert : advertPageData.getAdverts()) {
+                String oldCurrency = advert.getCurrency();
+                if(oldCurrency.equals("USD")){
+                    advert.setCurrency("UAH");
+                    double oldPrice = advert.getPrice();
+                    advert.setPrice(oldPrice * CurrencyConverter.sendGET());
+                }
+            }
+        }
         model.addAttribute("pageData", advertPageData);
 
         User user = new User();
